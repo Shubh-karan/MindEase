c0233c379dde35dc9964914119ba6d6d0cda9d21
Shubh <shubhkaran255@gmail.com>
2025-11-14 17:28:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/chat')
+def chat():
+    return render_template('chat.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
